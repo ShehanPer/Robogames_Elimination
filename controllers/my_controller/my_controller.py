5c359c90e884f7cfdc38adefa691a33e71cf1486
Shehan Sudeepa Perera <pereramss.22@uom.lk>
2025-02-09 21:29:58 +0530

    edited my controller

--- controllers/my_controller/my_controller.py
+++ controllers/my_controller/my_controller.py
@@ -15,9 +15,6 @@ def get_device(device_name):
         device.enable(timestep)
     return device
 
-# Get distance sensors (proximity)
-ps_names = ['ps5', 'ps2', 'ps7', 'ps0']
-proximity_sensors = [get_device(name) for name in ps_names]
 
 # Get ultrasonic sensors
 us_names = ['us0', 'us1', 'us2']  # Front, Left, Right
@@ -27,13 +24,30 @@ ultrasonic_sensors = [get_device(name) for name in us_names]
 def ultrasonic_to_distance(value):
     return value * 0.01  # Adjust scaling based on your lookup table
 
+L_motor = robot.getDevice('left wheel motor')
+R_motor = robot.getDevice('right wheel motor')
+
+L_motor.setPosition(float('inf'))
+R_motor.setPosition(float('inf'))
+
+L_encoderVal = get_device('left wheel sensor')
+R_encoderVal = get_device('right wheel sensor')
+def encoderPID():
+    #pid for straight moves
+def moveForward():
+    #move forward with pid for 0.25 meter
+def turnRight():
+    #turn 90 degree Right
+def turnLeft():
+    #turn 90 degree Left
+def turnReverse():
+    #turn 180 degree backward
+def moveBack():
+    #move 0.25 meter backward with pid
+
 # Main loop
 while robot.step(timestep) != -1:
-    # Read proximity sensors
-    for ps in proximity_sensors:
-        if ps:
-            print(f"{ps.getName()} reading: {ps.getValue()}")
-
+    
     # Read ultrasonic sensors
     for us in ultrasonic_sensors:
         if us:
